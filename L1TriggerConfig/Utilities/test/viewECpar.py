26b8c54276e2e9941b6db3cf9af3a99274b0d5e6
kkotov <kkotov@cern.ch>
2016-04-10 15:29:44 +0200

    db config

--- L1TriggerConfig/Utilities/test/viewECpar.py
+++ L1TriggerConfig/Utilities/test/viewECpar.py
@@ -12,18 +12,29 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 # The line below sets configuration from local file
 #process.load("L1Trigger.L1TCalorimeter.caloStage1Params_cfi")
 
-# Last option is readin local sqlite file
-from CondCore.DBCommon.CondDBSetup_cfi import CondDBSetup
-process.l1conddb = cms.ESSource("PoolDBESSource",
-       CondDBSetup,
-       connect = cms.string('sqlite:qwe.db'),
-       toGet   = cms.VPSet(
-            cms.PSet(
-                 record = cms.string('L1TMuonEndcapParamsRcd'),
-                 tag = cms.string("dummy")
-            )
-       )
+# Following block of lines accesses CondDB
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag.toGet = cms.VPSet(
+ cms.PSet(
+           record  = cms.string("L1TMuonEndcapParamsRcd"),
+           tag     = cms.string("L1TMuonEndcapParams_Stage2v0_hlt"),
+           connect = cms.string("frontier://FrontierPrep/CMS_CONDITIONS")
+          )
 )
+
+## Last option is readin local sqlite file
+#from CondCore.DBCommon.CondDBSetup_cfi import CondDBSetup
+#process.l1conddb = cms.ESSource("PoolDBESSource",
+#       CondDBSetup,
+#       connect = cms.string('sqlite:qwe.db'),
+#       toGet   = cms.VPSet(
+#            cms.PSet(
+#                 record = cms.string('L1TMuonEndcapParamsRcd'),
+#                 tag = cms.string("dummy")
+#            )
+#       )
+#)
 
 process.l1ecr = cms.EDAnalyzer("L1TEndcapReader")
 
